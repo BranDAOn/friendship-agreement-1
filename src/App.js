20af787b58ba0a78aa9c75a98d032e3238bb49e2
BranDAOn <93604605+BranDAOn@users.noreply.github.com>
2024-09-01 18:05:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ const FriendshipAgreement = () => {
 					I neither represent, warrant, nor promise anything to you by letting you use this agreement. 
 					I don't even know if its enforceable, although I think it should be (not legal advice). It's also hilarious.
 					Feel free to contribute to the code here: https://github.com/BranDAOn/friendship-agreement-1. Also, if you're
-					feeling generous, feel free to tip via Venmo at @Brandon-Ferrick or at brandonf.eth (if you're into crypto).
+					feeling generous, feel free to <b>tip via Venmo at @Brandon-Ferrick or at brandonf.eth</b> (if you're into crypto).
 					Feel free to ping me with any questions at brandon@bhfc.xyz. Enjoy, and don't do anything stupid.]</i></p>
 			</div>
 			<p className="mb-4 avoid-break">
