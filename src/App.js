24e60ac8051bdc9543701ccc1d4cbc1d992c3760
BranDAOn <93604605+BranDAOn@users.noreply.github.com>
2024-09-02 11:12:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const FriendshipAgreement = () => {
 			margin: 15,
 			filename: 'friendship_agreement.pdf',
 			image: { type: 'jpeg', quality: 0.98 },
-			html2canvas: { scale: 1.5 },
+			html2canvas: { scale: 2 },
 			jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait', compress: true }
 		};
 
